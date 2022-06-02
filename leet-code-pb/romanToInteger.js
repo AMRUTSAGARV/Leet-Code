34b2a2f10564e59ca0d5adeb51d63b33ed144bf1
Amrut Sagar <amrutsagar06@gmail.com>
2022-06-02 13:55:17 +0530

    longest common prefix

--- leet-code-pb/romanToInteger.js
+++ leet-code-pb/romanToInteger.js
@@ -29,3 +29,6 @@ var romanToInteger = function (s) {
 };
 
 console.log(romanToInteger("XIV"));
+// If the numerals are descending from left to right, largest to smallest, then they are added together. But if the numerals begin to ascend in order, then the larger number is subtracted by the preceding smaller number. For instance, in XVIV, X and V are added together to make 15. Because the I is followed by a larger number, V, it is subtracted from that number to make 4 and then added to the preceding XV to make 19.
+
+// Based on this rule, I will iterate from left to right. If the current symbol (cur) is less than the symbol to its right (next), then I will subtract the cur from the next and move 2 characters to the right. Otherwise, I will simply add them.

0e752504568c923923ff33e983834e1e9995e4b5
Amrut Sagar <amrutsagar06@gmail.com>
2022-06-08 17:55:50 +0530

    remove element

---
+++ leet-code-pb/removeElement.js
@@ -0,0 +1,13 @@
+const nums = [3, 2, 2, 3];
+const val = 3;
+
+var removeElement = function (nums, val) {
+  var len = nums.length;
+  var count = 0;
+  for (var i = 0; i < len; i++) {
+    if (nums[i] !== val) nums[count++] = nums[i];
+  }
+  return count;
+};
+
+console.log(removeElement());

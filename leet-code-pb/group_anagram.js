0441458e93cbfe573d84fa23249d42c19e5c5aa9
Amrut Sagar <amrutsagar06@gmail.com>
2022-08-15 16:46:46 +0530

    group anagram

---
+++ leet-code-pb/group_anagram.js
@@ -0,0 +1,14 @@
+var groupAnagrams = function (strs) {
+  let obj = {};
+  for (let i of strs) {
+    let key = i.split("").sort().join("");
+
+    if (!obj[key]) {
+      obj[key] = [i];
+    } else {
+      obj[key].push(i);
+    }
+  }
+  // console.log(obj)
+  return Object.values(obj);
+};

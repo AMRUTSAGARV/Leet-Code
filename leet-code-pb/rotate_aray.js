35a5ecf0a5efe450fc092f825e8ce5f9632fba7e
Amrut Sagar <amrutsagar06@gmail.com>
2022-09-19 20:49:11 +0530

    rotate array

---
+++ leet-code-pb/rotate_aray.js
@@ -0,0 +1,14 @@
+function newF(nums, k) {
+  k %= nums.length;
+  const reverse = (left, right) => {
+    const middle = Math.floor((left + right) / 2);
+    for (let i = left; i <= middle; i++) {
+      const temp = nums[i];
+      nums[i] = nums[right - (i - left)];
+      nums[right - (i - left)] = temp;
+    }
+  };
+  reverse(0, nums.length - 1);
+  reverse(0, k - 1);
+  reverse(k, nums.length - 1);
+}
